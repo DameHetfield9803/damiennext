7a6a2a127952766e0de21785fd19a12621369558
DameHetfield9803 <you@example.com>
2024-03-18 14:53:29 +0800

    sfsasad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-export default function Home(){
-  return(
-      <main className={"bg-black text-center container flex-auto mx-auto mt-6"}>
-        <div>
-          <h1 className={"text-6xl subpixel-antialiased"}><strong>This is a test page</strong></h1>
-            <Image src={'../.././public/next.svg'} alt={"Next JS"} width={400} height={250} className={"content-center container"}/>
+export default function Home() {
+  return (
+    <main className={"bg-black text-center container flex-auto mx-auto mt-5 min-w-full min-h-screen"}>
+      <div className={"container content-center"}>
+        <h1 className={"text-7xl subpixel-antialiased"}><strong>This is a test page</strong></h1>
+        <div className="mt-6 container bg-gradient-to-r from-sky-400 to-slate-700 max-w-fit flex-auto subpixel-antialiased rounded-lg">
+          <Image src={"../../next.svg"} width={800} height={600} alt={"Next JS"} />
         </div>
-      </main>
+      </div>
+    </main>
   );
 }
